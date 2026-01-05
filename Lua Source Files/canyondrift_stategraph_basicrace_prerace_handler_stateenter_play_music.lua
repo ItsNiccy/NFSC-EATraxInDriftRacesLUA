82c97bb539bc2340dfde1c6ddc3847db9823b141
ItsNiccy <MorningCoffee5525@protonmail.com>
2026-01-05 21:42:04 +1030

    Add files via upload

---
+++ Lua Source Files/canyondrift_stategraph_basicrace_prerace_handler_stateenter_play_music.lua
@@ -0,0 +1,6 @@
+h_E36628C8 = function(param0, param1, param2)
+	if param0.EventIconType == "canyondrift" then
+		Music.ForceCanyonMusic()
+		return
+	end
+end
---
+++ Lua Source Files/driftrace_stategraph_basicrace_racing_handler_stateenter_play_music.lua
@@ -0,0 +1,6 @@
+h_E97AD880 = function(param0, param1, param2)
+	if param0.EventIconType == "canyondrift" then
+		return
+	end
+	Music.ForceLicensedMusic()
+end
